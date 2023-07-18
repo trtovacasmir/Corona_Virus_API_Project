1e57c3c45530dc1e082a8b0a82d2a1a2c9e90e7b
Amil Kahrovic <kahrovicamil11@gmail.com>
2023-07-18 16:08:04 +0200

    + paginacija,prikaz drzava koje imaju zastavu

--- src/components/CovidStatus.js
+++ src/components/CovidStatus.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
 import axios from "axios";
+import { Box, Pagination } from "@mui/material";
 
-const MyComponent = () => {
+const CovidStats = () => {
   const [countries, setCountries] = useState([]);
   const [countryFlags, setCountryFlags] = useState([]);
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 10;
 
   useEffect(() => {
     fetchData();
@@ -42,7 +45,13 @@ const MyComponent = () => {
   };
 
   const renderCountryStats = (country) => {
-    const { country: countryName, continent, population } = country;
+    const {
+      country: countryName,
+      continent,
+      population,
+      cases,
+      deaths,
+    } = country;
     const countryFlag = countryFlags.find(
       (flag) => flag.name.common === countryName
     );
@@ -50,24 +59,66 @@ const MyComponent = () => {
     if (countryFlag) {
       return (
         <div className="country_card" key={countryName}>
-          <img src={countryFlag.flags.png} alt={countryName} />
-          <h3>{countryName}</h3>
-          <h3>{continent}</h3>
-          <h5>Population: {population}</h5>
+          <div className="image">
+            {" "}
+            <img src={countryFlag.flags.png} alt={countryName} />
+          </div>
+          <div className="country-and-continent">
+            <div className="continent">
+              {" "}
+              <h3>{continent}</h3>
+            </div>
+            <div className="country">
+              {" "}
+              <h3>{countryName}</h3>
+            </div>
+          </div>
+          <div className="stats">
+            <div className="population">
+              {" "}
+              <h5>Population: {population}</h5>
+            </div>
+            <div className="newCases">
+              <h5>New Cases: {cases.new || "0"}</h5>
+            </div>
+            <div className="deaths">
+              {" "}
+              <h5>Deaths: {deaths.total || "0"}</h5>
+            </div>
+          </div>
         </div>
       );
     } else {
-      console.log("bilo sta");
       return null;
     }
   };
 
+  const handleChangePage = (event, value) => {
+    setPage(value);
+  };
+
+  const total = countries.length;
+  const pageCount = Math.ceil(total / itemsPerPage);
+  const displayedCountries = countries.slice(
+    (page - 1) * itemsPerPage,
+    page * itemsPerPage
+  );
+
   return (
-    <div>
-      {countries.length > 0 &&
-        countries.map((country) => renderCountryStats(country))}
+    <div className="CovidCountries">
+      {displayedCountries.map((country) => renderCountryStats(country))}
+      <Box sx={{ mt: 2 }}>
+        <Pagination
+          className="pagination"
+          count={pageCount}
+          page={page}
+          onChange={handleChangePage}
+          showFirstButton
+          showLastButton
+        />
+      </Box>
     </div>
   );
 };
 
-export default MyComponent;
+export default CovidStats;
